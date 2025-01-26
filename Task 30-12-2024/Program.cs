c60df384d042a4b38fc3c529126f40c1ae6e2579
Astro-PC <amerxmostafa@gmail.com>
2025-01-26 05:36:23 +0200

    Add project files.

---
+++ Task 30-12-2024/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_30_12_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number of small carpets: ");
+            int smallCarpetsNumber = int.Parse(Console.ReadLine()!);
+            Console.Write("Number of large carpets: ");
+            int largeCarpetsNumber = int.Parse(Console.ReadLine()!);
+
+            Console.WriteLine("Price per small carpet is $25");
+            Console.WriteLine("Price per small carpet is $35");
+
+            int cost = smallCarpetsNumber * 25 + largeCarpetsNumber * 35;
+            double tax = cost * 0.06;
+
+            Console.WriteLine($"Cost: {cost}");
+            Console.WriteLine($"Tax: {tax}");
+
+            Console.WriteLine("=========================");
+
+            int offerValidDays = 30;
+
+            Console.WriteLine($"Total estimate: {cost + tax}");
+            Console.WriteLine($"This estimate is valid for {offerValidDays} days");
+        }
+    }
+}
